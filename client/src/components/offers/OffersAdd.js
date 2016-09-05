cca09d4202661a26d2a57d2385ce372523a06318
Filip Jerga <filip.jerga@netcentric.biz>
2016-09-05 11:37:49 +0200

    test file added, line removed in OffersAdd.js

--- client/src/components/offers/OffersAdd.js
+++ client/src/components/offers/OffersAdd.js
@@ -14,7 +14,6 @@ class OffersAdd extends React.Component {
     }
 
     openModal() {
-
         return this.setState({modalIsOpen: true});}
 
     afterOpenModal() {
@@ -27,14 +26,13 @@ class OffersAdd extends React.Component {
     handleSubmit(e){
 
         e.preventDefault();
-
             var offer = {};
 
             offer["companyName"] = this.refs.companyName.value;
             offer["customerAdress"] = this.refs.customerAdress.value;
             offer["item"] = this.refs.item.value;
             const {dispatch} = this.props;
-        
+
             dispatch(actions.addOffer(offer));
             this.setState({modalIsOpen: false});
     }
@@ -55,15 +53,15 @@ class OffersAdd extends React.Component {
                 onAfterOpen={this.afterOpenModal}
                 onRequestClose={this.closeModal.bind(this)}
                        style={customStyles}>
-        
+
                 <h2 ref="subtitle">Add Offer- FOR_FAST ADDING TO DB, FOR TEST PURPOSES ONLY</h2>
                     <Link onClick={this.closeModal.bind(this)} to = "/" > Return </Link>
                     <form onSubmit={this.handleSubmit.bind(this)}>
-            
+
                     <input type="text" id="fname" ref="companyName" placeholder="Company Name"/>
                         <input type="text" id="lname" ref="customerAdress" placeholder="Adress"/>
                         <input type="text" id="lname" ref="item" placeholder="Product"/>
-            
+
                         <button>Add</button>
                     </form>
                 </Modal>
---
+++ client/src/components/offers/testFile.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {connect} from 'react-redux';
+
+class TestFile extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+        render(){
+            return(
+              <div> Just Testing File! </div>
+            )
+        }
+}
+
+export default TestFile;
